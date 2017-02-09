c8cced7e4fe225565959eec9ec8b8e1baa862756
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2017-02-09 15:17:39 +0700

    use import:use instead of import:source

--- progress.bash
+++ progress.bash
@@ -1,6 +1,6 @@
-if ! declare -f import:source &>/dev/null; then
+if ! declare -f import:use &>/dev/null; then
     _base_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-    source $_base_dir/vendor/github.com/reconquest/import.bash/import.bash
+    use $_base_dir/vendor/github.com/reconquest/import.bash/import.bash
 fi
 
 import:use "github.com/reconquest/coproc"
